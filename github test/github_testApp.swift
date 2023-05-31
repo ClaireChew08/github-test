caf7773b1c2c120e5adcabae6663fb026cd28374
ClaireChew08 <chew.rh.claire08@gmail.com>
2023-05-31 11:08:24 +0800

    Initial Commit

---
+++ github test/github_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  github_testApp.swift
+//  github test
+//
+//  Created by rgs1 on 31/5/23.
+//
+
+import SwiftUI
+
+@main
+struct github_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
